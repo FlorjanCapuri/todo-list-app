daec0e51e1d889d7882335498d9ca66fc7c43002
FlorjanCapuri <florjancapuri@gmail.com>
2022-11-02 20:29:43 +0200

    Version 1.6 Created a description for my application

--- react-todo-list-app/src/App.js
+++ react-todo-list-app/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Footer from './components/Footer';
 import Title from './components/Title';
 import TodoList from './components/TodoList';
+import Description from './components/Description';
 
 function App() {
   return (
-    <div>
+    <div className='general'>
       <div className='title'><Title /></div>
-      <div className="todo-app"><TodoList /></div>
-      <div className="footer"><Footer /></div>
+      <div className='todo-app'><TodoList /></div>
+      <div className='description'><Description /></div>
+      <div className='footer'><Footer /></div>
     </div>
   );
 }
---
+++ react-todo-list-app/src/components/Description.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Description() {
+    return (
+        <div>
+            <h3>Description</h3>
+            <div className='app-description'>
+                <p>
+                    ToDo List Application is a task management app to help you stay organized and manage your plans.
+                    <br></br>
+                    You can use ToDo List Application to make shopping lists or task lists, take notes, record collections, plan an event, or set reminders to increase your productivity and focus on what matters to you.
+                    <br></br>
+                    ToDo List Application makes it easy to stay organized and manage your life.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Description
